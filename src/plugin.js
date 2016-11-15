a35f5727c11332b0a53cc9f4ef70a5c6df12c3f8
Sean Jestadt <sjestadt@gmail.com>
2016-11-14 17:27:31 -0800

    Changes

--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,9 @@
 import videojs from 'video.js';
 
+const Button = videojs.getComponent('Button');
+const Component = videojs.getComponent('Component');
+const Tech = videojs.getTech('Tech');
+
 // Default options for the plugin.
 const defaults = {};
 
@@ -16,6 +20,21 @@ const defaults = {};
  */
 const onPlayerReady = (player, options) => {
   player.addClass('vjs-kodi');
+  console.log({
+    'vjs-kodi player': player,
+    options: options
+  })
+  if(!options.kodi){
+    return false;
+  }
+  player.controlBar.kodi = player.controlBar.addChild('kodiButton', {
+    kodi: options.kodi
+  });
+
+  player.controlBar.el().insertBefore(
+      player.controlBar.kodi.el(),
+      player.controlBar.fullscreenToggle.el()
+  );
 };
 
 /**
@@ -35,6 +54,148 @@ const kodi = function(options) {
     onPlayerReady(this, videojs.mergeOptions(defaults, options));
   });
 };
+
+class KodiButton extends Button {
+  constructor(player, options) {
+    super(player, options);
+    this.kodi = player.getChild('KodiPlayer');
+  }
+
+  buildCSSClass() {
+    return 'vjs-kodi-button';
+  }
+
+
+  handleClick() {
+    this.kodi.trigger('toggle');
+  }
+}
+
+Component.registerComponent('KodiButton', KodiButton);
+
+class KodiPlayer extends Component {
+  constructor(player, option) {
+    super(player, option);
+
+
+    this.on('stopKodi', this.handleStopKodi);
+    this.on('startKodi', this.handleStartKodi);
+    this.on('toggle', this.handleToggle);
+    this.on('kodiAvailable', this.handleKodiAvailable);
+    this.on('kodiUnavailable', this.handleKodiUnavailable);
+    this.on('kodiConnected', this.handleKodiConnected);
+    this.on('kodiDisconnected', this.handleKodiDisconnected);
+
+    this.casting = false;
+
+  }
+
+  handleToggle() {
+    if (this.casting) {
+      this.trigger('stopKodi');
+    } else {
+      this.trigger('startKodi');
+    }
+  }
+
+  handleKodiConnected() {
+    videojs.log('handleKodiConnected');
+    this.player_.textTrackDisplay.hide();
+  }
+
+  handleKodiDisconnected() {
+    videojs.log('handleKodiDisconnected');
+    this.player_.textTrackDisplay.show();
+  }
+
+  handleKodiAvailable() {
+    videojs.log('handleKodiAvailable');
+  }
+
+  handleKodiUnavailable() {
+    videojs.log('handleKodiUnavailable');
+  }
+
+  handleStopKodi() {
+    videojs.log('onStopKodi');
+    //this.stopKodiing();
+  }
+
+  handleStartKodi() {
+    videojs.log('onStartKodi');
+    /*if (this.apiInitialized) {
+      if (this.apiSession) {
+        this.onSessionSuccess(this.apiSession);
+      } else {
+        chrome.cast.requestSession(this.onSessionSuccess.bind(this),
+            this.castError);
+      }
+    } else {
+      videojs.log('Session not initialized');
+    }*/
+  }
+}
+
+videojs.registerComponent('KodiPlayer', KodiPlayer);
+
+class KodiTech extends Tech {
+  constructor(options, ready) {
+    super(options, ready);
+
+    this.featuresVolumeControl = true;
+    this.movingMediaElementInDOM = false;
+    this.featuresFullscreenResize = false;
+    this.featuresProgressEvents = true;
+    this.source = options.source;
+    this.kodi = options.kodi;
+
+
+  }
+
+  dispose() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+    this.textTracks().removeEventListener('change', this.onTrackChangeHandler);
+    super.dispose();
+  }
+
+  createEl() {
+    const kodi = this.options().kodi;
+    const name = "Kodi";
+    const el = videojs.createEl('div');
+
+    el.className = 'vjs-tech vjs-tech-kodi';
+    el.innerHTML = `
+<div class=\'kodi-casting-image\' style=\'background-image: url('')\'></div>
+<div class=\'kodi-casting-overlay\'>
+<div class=\'kodi-casting-information\'>
+<div class=\'kodi-casting-icon\'>&#58880</div>
+<div class=\'kodi-casting-description\'><small>Streaming to</small><br>${name}</div>
+</div>
+</div>`;
+    return el;
+  }
+
+  play() {
+    if (!this.kodi) {
+      return;
+    }
+
+    console.log(this);
+    return;
+    this.kodi.playFile();
+
+    if (this._paused) {
+      this.apiMedia.play(null,
+          this.onPlaySuccess.bind(this),
+          this.onError);
+      this._paused = false;
+    }
+  }
+}
+
+videojs.registerComponent('KodiTech', KodiTech);
 
 // Register the plugin with video.js.
 videojs.plugin('kodi', kodi);
